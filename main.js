17d46ab7f1f17e087af9c00e8d9dc04129a827fd
Tinnker <857288908@qq.com>
2021-02-25 00:56:20 +0800

    D 2.25

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ module.exports.loop = function () {
             tower.attack(closestHostile);
         }
         else {
-            var repairTime = 2;
+            var repairTime = 1;
             if(!(Game.time % repairTime)) {
                 var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: (structure) => {
